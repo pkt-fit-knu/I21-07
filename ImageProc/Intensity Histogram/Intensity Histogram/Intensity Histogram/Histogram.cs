95b3354073cd4ebcd34e429387724102420010f1
Viktor Kozorez <vityakozorez1997@gmail.com>
2016-03-14 20:23:28 +0200

    Building a histogram of the image pixels intensities

---
+++ ImageProc/Intensity Histogram/Intensity Histogram/Intensity Histogram/Histogram.cs
@@ -0,0 +1,44 @@
+﻿using System.Drawing;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ImageToHistogram
+{
+    internal class Histogram
+    {
+        private int[] intensities;
+
+        public Histogram()
+        {
+            this.intensities = new int[256];
+        }
+
+        public void SetIntensities(Bitmap image)
+        {
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    int intensity = (int)(RGBConstants.RedMultiplier * image.GetPixel(x, y).R +
+                                          RGBConstants.GreenMultiplier * image.GetPixel(x, y).G +
+                                          RGBConstants.BlueMultiplier * image.GetPixel(x, y).B);
+
+                    this.intensities[intensity]++;
+                }
+            }
+        }
+
+        public void DrawHistogram(Chart chart)
+        {
+            chart.Series["Intensity"].Points.Clear();
+
+            for (int i = 0; i < this.intensities.Length; i++)
+            {
+                DataPoint dataPoint = new DataPoint();
+
+                dataPoint.SetValueXY(i, this.intensities[i]);
+
+                chart.Series["Intensity"].Points.Add(dataPoint);
+            }
+        }
+    }
+}
