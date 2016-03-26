b85e9cc17547c9d876977ff51fb5049dc81230da
Viktor Kozorez <vityakozorez1997@gmail.com>
2016-03-26 21:27:50 +0200

    update

--- ImageProc/Intensity Histogram/Intensity Histogram/Intensity Histogram/RGBConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ImageToHistogram
-{
-    static internal class RGBConstants
-    {
-        public const double RedMultiplier = 0.3;
-        public const double GreenMultiplier = 0.59;
-        public const double BlueMultiplier = 0.11;
-    }
-}
